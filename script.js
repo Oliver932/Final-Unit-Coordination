e17e2c4964777b8b39f7bc67899f4133e8192aaa
Oliver932 <76821440+Oliver932@users.noreply.github.com>
2021-02-03 12:25:07 +0000

    grouping

--- script.js
+++ script.js
@@ -35,16 +35,16 @@ addEventListener('click', function(event) {
     }
 
     if (event.shiftKey == true) {
-        units[team].push(new Unit(event.clientX, event.clientY, team, 'Commander', 20, 1, 300, 300));
+        units[team].push(new Unit(event.clientX, event.clientY, team, 'Commander', 6, 0.5, 300, 300));
     }else {
-        units[team].push(new Unit(event.clientX, event.clientY, team, 'Soldier', 10, 3, 100, 100));
+        units[team].push(new Unit(event.clientX, event.clientY, team, 'Soldier', 6, 3, 100, 100));
     }
 
  
 })
 
 const dimension = 10;
-
+const offset = 10;
 
 var img = new Image();
 img.src = 'Modern Armor.png';
@@ -54,8 +54,12 @@ img.src = 'Modern Armor.png';
 // }
 
 teamColours = {
-    'Oli': 'cyan',
-    'Hazza': 'red'
+    'Oli': {
+        'moving' :'cyan',
+        'static' :'blue'},
+    'Hazza': {
+        'moving':'orange',
+        'static':'red'}
 };
 
 var units = {
@@ -80,13 +84,13 @@ var Unit = function (x, y, team, rank, size, sMax, mMax, hMax) {
     this.morale = mMax;
     this.health = hMax;
 
-    this.engaged = false;
+    this.status = 'moving';
 
     this.draw = function () {
 
         c.beginPath();
         c.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
-        c.fillStyle = teamColours[this.team];
+        c.fillStyle = teamColours[this.team][this.status];
         c.fill();
 
         c.strokeStyle = 'black';
@@ -102,7 +106,7 @@ var Unit = function (x, y, team, rank, size, sMax, mMax, hMax) {
 
         this.draw();
 
-        if (this.sMax > 0 && this.engaged == false) {
+        if (this.sMax > 0) {
 
             this.move(this.chooseTarget());
         }
@@ -114,113 +118,221 @@ var Unit = function (x, y, team, rank, size, sMax, mMax, hMax) {
         return Math.sqrt(((x - this.x)**2) + ((y - this.y)**2))
     }
 
+    this.vectorTOangle = function (dx,dy) {
+
+        var angle = undefined;
+
+        if (dx != 0 || dy != 0) {
+
+            if (dx == 0) {
+
+                if (dy > 0) {
+                    angle = Math.PI;
+                } else {
+                    angle = 0;
+                }
+            } else if (dy == 0) {
+
+                if (dx > 0) {
+                    angle = 0.5 * Math.PI;
+                } else {
+                    angle = 1.5 * Math.PI;
+                }
+            } else {
+                angle = Math.atan(dx / dy) * -1;
+
+                if (Math.sign(dy) == 1) {
+                    angle += Math.PI;
+                } else if (Math.sign(dy) == -1) {
+                    angle += 2* Math.PI;
+                }
+                
+            }
+
+        }
+
+        console.log(angle)
+        return angle;
+    }
+
     this.chooseTarget = function () {
 
         var x = 0;
         var y = 0;
 
-        var offset = 0;
-        var accentuate = 2;
+        var clusterAttraction = 0;
+        var clusterRepulsion = 0.2;
+
+        var accentuate = 12;
+        // var limit = Math.sin(1/accentuate);
 
         for (const team in units) {
             if (Object.hasOwnProperty.call(units, team)) {
 
-                if (team != this.team) {
-                    
-                    for (let i = 0; i < units[team].length; i++) {
 
-                        
-                        var opponent = units[team][i];
+                    
+                for (let i = 0; i < units[team].length; i++) {
+
+                    var opponent = units[team][i];
+
+                    if (this != opponent) {
+
                         var distance = this.distance(opponent.x, opponent.y)
 
-                        if (distance > this.size + opponent.size) {
+                        var xMultiplier = 1;
+                        var yMultiplier = 1;
 
-                            var xMultiplier = 1;
-                            var yMultiplier = 1;
-
-                            if (opponent.x < this.x) {
+                        if (opponent.x < this.x) {
                                 xMultiplier = -1
-                            }
+                        }
                             
-                            if (opponent.y < this.y) {
+                        if (opponent.y < this.y) {
                                 yMultiplier = -1
-                            }
+                        }
 
-                            var distFunc = Math.sin(1/(accentuate*((distance * 2 * Math.PI)/diagonal)) + 0.317 + offset);
+                        var distFunc = Math.sin(1/(accentuate * ((distance - this.size - opponent.size + offset)/diagonal)));
 
-                            if (distFunc > 0) {
+                        if (distFunc > 0) {
+                            var xRatio = Math.abs((opponent.x - this.x)/((opponent.y - this.y) + (opponent.x - this.x)));
+                            var yRatio = 1 - xRatio;
 
-                                var xRatio = Math.abs((opponent.x - this.x)/((opponent.y - this.y) + (opponent.x - this.x)));
-                                var yRatio = 1 - xRatio;
+                            if (opponent.morale < (this.morale * 3) && team != this.team) {
 
-                                if (opponent.morale < (this.morale * 3)) {
+                                x +=(this.morale/opponent.morale)*distFunc * xMultiplier * xRatio;
+                                y += (this.morale/opponent.morale)*distFunc * yMultiplier * yRatio;
 
-                                    x +=(this.morale/opponent.morale)*distFunc * xMultiplier * xRatio;
-                                    y += (this.morale/opponent.morale)*distFunc * yMultiplier * yRatio;
+                            } else if (team != this.team){
 
-                                } else {
+                                x -= (opponent.morale/this.morale)*distFunc * xMultiplier * xRatio;
+                                y -= (opponent.morale/this.morale)*distFunc * yMultiplier * yRatio;
 
-                                    x -= (opponent.morale/this.morale)*distFunc * xMultiplier * xRatio;
-                                    y -= (opponent.morale/this.morale)*distFunc * yMultiplier * yRatio;
-                                }
+                            } else if (team == this.team && (opponent.status == 'static' || this.status == 'static')) {
+
+                                x -= (opponent.morale/this.morale)*distFunc * xMultiplier * xRatio * clusterRepulsion;
+                                y -= (opponent.morale/this.morale)*distFunc * yMultiplier * yRatio * clusterRepulsion;
+
+                            } else if (opponent.status == 'moving' && team== this.team && this.status == 'moving') {
+
+                                x +=(this.morale/opponent.morale)*distFunc * xMultiplier * xRatio * clusterAttraction;
+                                y += (this.morale/opponent.morale)*distFunc * yMultiplier * yRatio * clusterAttraction;
 
                             }
 
                         }
-
                     }
+
                 }
+                
             
             }
         }
     
-    return {'x':x, 'y':y}
+    
+    return {'x':x * Math.random(), 'y':y * Math.random()}
         
     }
 
     this.move = function(score) {
 
+        console.log(score.x, score.y);
         var dx = 0;
         var dy = 0;
 
+
         if (score.x != 0 && score.y != 0) {
 
-            // var xMultiplier = 1;
-            // var yMultiplier = 1;
-
-            // if (score.x < 0) {
-            //     xMultiplier = -1
-            // }
-                        
-            // if (score.y < 0) {
-            //     yMultiplier = -1
-            // }
-
-
-            var ratio = score['x'] / score['y'];
-            dy = Math.sqrt((this.sMax ** 2)/((ratio **2) + 1));
-            dx = ratio * dy;
-
-            if (score.y < 0) {
-                dy *= -1;
-                dx *= -1;
-            }
-
-            // dx *= xMultiplier;
-            // dy *= yMultiplier;
-
-
+            var ratio = Math.abs(score['x'] / score['y']);
+            dy = Math.sign(score.y) * Math.sqrt((this.sMax ** 2)/((ratio **2) + 1));
+            dx = Math.sign(score.x) * Math.abs(ratio * dy);
 
         
         } else if (score.x != 0 && score.y == 0) {
 
-            dx = this.sMax;
+            dx = this.sMax * Math.sign(score.x);
 
         } else if (score.x == 0 && score.y != 0) {
 
-            dy = this.sMax;
+            dy = this.sMax * Math.sign(score.y);
+
         }
 
+
+        for (const team in units) {
+            if (Object.hasOwnProperty.call(units, team)) {
+                    
+                for (let i = 0; i < units[team].length; i++) {
+                        
+                    var opponent = units[team][i];
+                        
+                    if (this.distance(opponent.x, opponent.y) - sMax <= offset + this.size + opponent.size) {
+
+                        var xDirection = Math.sign(opponent.x - this.x);
+                            
+                        var xDist = Math.abs(opponent.x - this.x) - this.size - opponent.size - offset;
+
+                        if (xDist < 0 ){
+                            if (team != this.team) {
+                                xDist = 0;
+                            }
+                        }
+
+                        if (Math.sign(dx) == xDirection && Math.abs(dx) > xDist) {
+                            dx = xDist * Math.sign(dx)
+                        }
+
+                        var yDirection = Math.sign(opponent.y - this.y);
+                            
+                        var yDist = Math.abs(opponent.y - this.y) - this.size - opponent.size - offset;
+
+                        if (yDist < 0){
+                            yDist = 0;
+                        }
+                            
+                        if (Math.sign(dy) == yDirection && Math.abs(dy) > yDist) {
+                                dy = yDist  * Math.sign(dy);
+                        }
+                        
+
+                    }
+                }
+            }
+        }
+        
+
+        // for (const team in units) {
+        //     if (Object.hasOwnProperty.call(units, team)) {
+                    
+        //         for (let i = 0; i < units[team].length; i++) {
+                        
+        //             var opponent = units[team][i];
+                        
+        //             if (this != opponent) {
+
+        //                 var distance = Math.sqrt(((opponent.x-(this.x + dx)) ** 2) + ((opponent.y - (this.y + dy)) ** 2))
+        //                 if (distance < this.size + opponent.size + offset) {
+
+        //                     if (dx != 0 && dy != 0) {
+
+        //                         var vDist = this.distance(opponent.x, opponent.y)
+        //                         var sDist = this.size + opponent.size + offset
+        //                         var angle = Math.acos(((this.sMax ** 2) + (vDist ** 2) - (sDist **2 ))/ (2 * vDist * this.sMax));
+        //                         angle += 1;
+
+        //                     }
+
+        //                 }
+        //             }
+        //         }
+        //     }
+        // }
+
+        if (dy == 0 && dx == 0){
+            this.status = 'static';
+        } else {
+            this.status = 'moving';
+        }
+
+        console.log(this.vectorTOangle(dx, dy));
 
         this.x += dx
         this.y += dy
